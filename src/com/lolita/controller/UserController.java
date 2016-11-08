a6e627a98afdc3d4181c2fae3eb355f38d5074cb
AzusA-Wu <303530094@qq.com>
2016-11-08 16:51:36 +0800

    fix some bug

--- src/com/lolita/controller/UserController.java
+++ src/com/lolita/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.lolita.controller;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
